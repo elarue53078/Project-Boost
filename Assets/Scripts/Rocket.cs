fc374e2c3e201ccdcd14d5b77e3cf469956e0c93
Eric LaRue <safespace53078@gmail.com>
2019-09-13 00:22:39 -0500

    Added thrust behavior
    Added thrust behavior to the space bar input

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
+    Rigidbody rocketRB;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rocketRB = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -22,6 +24,7 @@ public class Rocket : MonoBehaviour
         if (Input.GetKey(KeyCode.Space)) // can thrust while rotating
         {
             print("Thrusting.");
+            rocketRB.AddRelativeForce(Vector3.up);
         }
 
         if (Input.GetKey(KeyCode.A))
