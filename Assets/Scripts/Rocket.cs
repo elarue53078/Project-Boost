ae198b8208fc3428353cc5eac7635f83128c4ed7
Eric LaRue <safespace53078@gmail.com>
2020-04-14 18:06:09 -0500

    Refactor audio sources, extracted methods
    Refactor audio sources, extracted methods

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -2,30 +2,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Rocket : MonoBehaviour
 {
     Rigidbody rocketRB;
-    AudioSource rocketThrust;
+    AudioSource rocketAudio;
     [SerializeField] float mainThrust = 100f;
     [SerializeField] float rcsThrust = 100f;
+    [SerializeField] AudioClip mainEngine;
+    [SerializeField] AudioClip rocketExplosion;
+    [SerializeField] AudioClip levelLoad;
+
+    enum State {Alive, Dying, Transcending}
+    State state = State.Alive;
 
     // Start is called before the first frame update
     void Start()
     {
         rocketRB = GetComponent<Rigidbody>();
-        rocketThrust = GetComponent<AudioSource>();
+        rocketAudio = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Thrust();
-        Rotate();
+        if(state == State.Alive)
+        {
+            RespondToThrustInput();
+            RespondToRotateInput();
+        }
     }
 
     private void OnCollisionEnter(Collision collision)
     {
+        if(state != State.Alive) { return; }
+
         print("collided");
         switch (collision.gameObject.tag)
         {
@@ -33,33 +45,69 @@ public class Rocket : MonoBehaviour
                 //do nothing
                 print("Okay");
                 break;
+            case "Finish":
+                StartSuccess();
+                break;
             default:
-                print("dead");
+                StartExplosion();
                 break;
 
         }
     }
 
-    private void Thrust()
+    private void StartExplosion()
+    {
+        print("dead");
+        state = State.Dying;
+        rocketAudio.Stop();
+        rocketAudio.PlayOneShot(rocketExplosion);
+        Invoke("PlayerDie", 2f); //parameterize time
+    }
+
+    private void StartSuccess()
+    {
+        print("Hit finish");
+        state = State.Transcending;
+        rocketAudio.Stop();
+        rocketAudio.PlayOneShot(levelLoad);
+        Invoke("LoadNextScene", 1f); //parameterize time
+    }
+
+    private void PlayerDie()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    private void LoadNextScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    private void RespondToThrustInput()
     {
         float thrustSpeed = mainThrust * Time.deltaTime;
 
         if (Input.GetKey(KeyCode.Space)) // can thrust while rotating
         {
-            print("Thrusting.");
-            rocketRB.AddRelativeForce(Vector3.up * thrustSpeed);
-            if (!rocketThrust.isPlaying) // no layering
-            {
-                rocketThrust.Play();
-            }
+            ApplyThrust(thrustSpeed);
         }
         else
         {
-            rocketThrust.Stop();
+            rocketAudio.Stop();
         }
     }
 
-    private void Rotate()
+    private void ApplyThrust(float thrustSpeed)
+    {
+        print("Thrusting.");
+        rocketRB.AddRelativeForce(Vector3.up * thrustSpeed);
+        if (!rocketAudio.isPlaying) // no layering
+        {
+            rocketAudio.PlayOneShot(mainEngine);
+        }
+    }
+
+    private void RespondToRotateInput()
     {
         rocketRB.freezeRotation = true;
         float rotationSpeed = rcsThrust * Time.deltaTime;
